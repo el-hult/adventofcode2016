5970e851724dec3ee76936b0a2e9084f74ecc7fc
Ludvig Hult <ludvig.hult@gmail.com>
2016-12-07 16:41:08 +0100

    Task A in the bank. I found it quite difficult and it took ~2 hours to solve it. I got somewhat fooled by the haskell syntax for pattern matching. :(

---
+++ Day7/Day7.hs
@@ -0,0 +1,64 @@
+import System.IO
+import Data.List
+import Data.List.Split (splitOneOf)
+import Data.Map (toList, fromListWith)
+
+parseString :: [Char] -> Bool
+
+parseString (a:b:c:d:tail)
+ | isAbba a b c d = True
+ | otherwise = parseString (b:c:d:tail)
+parseString _ = False
+
+isAbba :: Char -> Char -> Char -> Char -> Bool
+isAbba a b c d
+ | a == b = False
+ | c == d = False
+ | (a == d) && (b == c) = True
+ | otherwise = False
+
+cutSectionOne :: [Char] -> [Char]
+cutSectionOne (x:xs)
+ | x == '[' = xs
+ | otherwise = cutSectionOne xs
+
+data Level = Good | Bad | Meh deriving(Eq,Show)
+
+evalPair :: (Bool,String) -> Level
+evalPair (b,s)
+ | b && (parseString s) = Good
+ | not(b) && (parseString s) = Bad
+ | otherwise = Meh
+
+evalLine :: Bool -> [(Bool,String)] -> Bool
+evalLine s (x:y:xs)
+ | evalPair x == Bad = False
+ | evalPair x == Good = evalLine True (y:xs)
+ | evalPair x == Meh = evalLine s (y:xs)
+
+evalLine s (x:_)
+ | evalPair x == Bad = False
+ | evalPair x == Good = True
+ | evalPair x == Meh = s
+
+--evalLine s _ = s
+
+taskA  :: [Char] -> IO () 
+taskA s = do
+ let splitLists = map (zip (cycle [True,False] )) $ map (splitOneOf "[]" ) $ lines s
+ let goodBad = map (evalLine False) splitLists
+
+ putStrLn $ show $ length $ filter id goodBad
+ --putStrLn $ show $ take 10 (zip3 goodBad splitLists (lines s))
+
+taskB  :: [Char] -> IO () 
+taskB s = do
+ putStrLn "hej"
+
+main =
+ readFile "input.txt" >>= \inString ->
+  putStrLn "Answer to A:" >>
+  taskA inString >> -- (151 is too high...) (47 is too low)
+  putStrLn "Answer to B:" >>
+  taskB inString
+  
