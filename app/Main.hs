8d1da04132a48176b722713f5f79c7ba623af3dc
Ludvig Hult <ludvig.hult@gmail.com>
2021-07-01 19:20:38 +0200

    Day3 into stack project

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,13 @@
 module Main where
 
-import Lib (day1,day2)
+import Lib (day1,day2,day3)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
 runDay s = case readMaybe s of
     Just 1->day1
     Just 2->day2
+    Just 3->day3
     _ -> print "invalid day"
 
 main :: IO ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib where
 import qualified Day1 as D1
 import qualified Day2 as D2
+import qualified Day3 as D3
 
 day1 = D1.main
 day2 = D2.main
+day3 = D3.main
