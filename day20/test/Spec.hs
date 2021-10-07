8e94cb33ee6a6c1d1d9e9ac62eb08e0256909970
Ludvig Hult <ludvig.hult@gmail.com>
2021-10-07 16:45:38 +0200

    removed test autodiscovery to make the vscode tooling better

--- day20/test/Spec.hs
+++ day20/test/Spec.hs
@@ -1,3 +1,13 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
 
--- This file is a dummy to start the HSpec test discovery. See https://hspec.github.io/hspec-discover.html
+import Day20
+import Test.Hspec
+
+testStr = "5-8\n0-2\n4-7"
+
+spec :: Spec
+spec = do
+  it "parses the list" $ makeBlacklist testStr `shouldBe` Blacklist [(0, 2), (4, 8)]
+  it "counts the blocked ones correctly" $ totalBlocked (makeBlacklist testStr) `shouldBe` 8
+
+main = hspec $ spec
