2339a65e073a8a0924e07ab978779d231c1a1416
Ludvig Hult <ludvig.hult@gmail.com>
2025-08-16 21:44:48 +0200

    day2 done refactor

--- day02/test/Spec.hs
+++ day02/test/Spec.hs
@@ -1,3 +1,8 @@
+import Day2 as D2
+import Data.Map ((!?))
+import Test.HUnit
+
+testDay2 :: Test
 testDay2 =
   TestList
     [ TestLabel "testX" $ TestCase (assertEqual "Nice show" "(-1,0)" (show $ D2.move D2.U (0, 0))),
@@ -7,3 +12,6 @@ testDay2 =
       TestLabel "testX" $ TestCase (assertEqual "Nice show" "([5,1],(0,0))" (show $ D2.addNextButton D2.numPadA ([D2.Five], (1, 1)) [D2.U, D2.U, D2.U, D2.L, D2.L])),
       TestLabel "testX" $ TestCase (assertEqual "Nice show" "[2,1]" (show $ D2.addAllButtons D2.numPadA (1, 1) [[D2.U, D2.U, D2.U], [D2.L, D2.L]]))
     ]
+
+main :: IO ()
+main =  (runTestTT $ TestList [testDay2]) >>= print
